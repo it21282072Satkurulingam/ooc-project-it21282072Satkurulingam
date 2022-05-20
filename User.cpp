c32d2bbd2c556befc0a9908fbb890416d57764fa
it21282072Satkurulingam <99562725+it21282072Satkurulingam@users.noreply.github.com>
2022-05-20 21:40:49 +0530

    Add files via upload

---
+++ User.cpp
@@ -0,0 +1,11 @@
+#include"User.h"
+
+User::User(string uName, string add, string mail, int no){
+  username=uName;
+  address=add;
+  email=mail;
+  contactNo=no;
+}
+void User::displayDetails(){
+  
+}
